a0ef7032486a987d5d5b298466b3a0edd3a014e3
jeffball <jeffball@jeffballs-mbp.lan>
2023-08-18 07:25:51 -0700

    Add quote generator component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,20 @@
+import React, { useState } from 'react'
+
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
-import { GradientBackgroundCon } from '@/components/QuoteGenerator/QuoteGeneratorElements'
+
+// Components
+import { BackgroundImage1, BackgroundImage2, FooterCon, FooterLink, GenerateQuoteButton, GenerateQuoteButtonText, GradientBackgroundCon, QuoteGeneratorCon, QuoteGeneratorInnerCon, QuoteGeneratorSubTitle, QuoteGeneratorTitle, RedSpan } from '@/components/QuoteGenerator/QuoteGeneratorElements'
+
+
+// Assets
+import Clouds1 from '../assets/cloud-and-thunder.png'
+import Clouds2 from '../assets/cloudy-weather.png'
 
 export default function Home() {
+  const [numberOfQuotes, setNumberOfQuotes] = useState<Number | null>(0);
+
   return (
     <>
       <Head>
@@ -15,6 +26,48 @@ export default function Home() {
       {/* Background */}
       <GradientBackgroundCon>
         
+        {/* Quote Generator Modal Pop-Up */}
+
+        {/* Quote Generator */}
+        <QuoteGeneratorCon>
+          <QuoteGeneratorInnerCon>
+            <QuoteGeneratorTitle>
+              Daily Inspiration Generator
+            </QuoteGeneratorTitle>
+            <QuoteGeneratorSubTitle>
+              Looking for a splash of inspiration? Generate a quote card with a random inspirational quote provided by <FooterLink href="https://zenquotes.io/" target="_blank" rel="noopener noreferrer">ZenQuotes API</FooterLink>.
+            </QuoteGeneratorSubTitle>
+            <GenerateQuoteButton>
+              <GenerateQuoteButtonText onClick={null}>
+                Make a Quote
+              </GenerateQuoteButtonText>
+            </GenerateQuoteButton>
+          </QuoteGeneratorInnerCon>
+        </QuoteGeneratorCon>
+
+        {/* Background Images */}
+        <BackgroundImage1
+          src={Clouds1}
+          height="300"
+          alt="cloudybackground1"
+        />
+
+        <BackgroundImage2
+          src={Clouds2}
+          height="300"
+          alt="cloudybackground2"
+        />
+
+        {/* Footer */}
+        <FooterCon>
+          <>
+            Quotes Generated: {numberOfQuotes}
+            <br />
+            Developer with <RedSpan>♥</RedSpan> by <FooterLink href="https://www.linkedin.com/in/jeff-ball-092228162/" target="_blank" rel="noopener noreferrer"> @jeffpball
+            </FooterLink>
+          </>
+        </FooterCon>
+
       </GradientBackgroundCon>
     </>
   )
